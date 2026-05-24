cf5ce71c5652cf2e9fa9a6ea721824684bc230c8
SYLENTH0215 <zayniddinovdavron420@gmail.com>
2026-05-24 22:53:22 +0500

    Organize configuration variables in config.py

--- config.py
+++ config.py
@@ -1,11 +1,16 @@
 import os
-BOT_TOKEN = "8701673908:AAGk2e6J8X79AvVE2VuajywwiuvnK_GhqC8" 
-ADMIN_ID = 8103843122 
+
+# Telegram
+BOT_TOKEN = "8701673908:AAGk2e6J8X79AvVE2VuajywwiuvnK_GhqC8"
+ADMIN_ID = 8103843122
 REQUIRED_CHANNEL = "https://t.me/edugrandsuz"
 
-FLOOD_RATE = 1.5   
-BAN_THRESHOLD = 5      
+# Bot sozlamalari
+FLOOD_RATE = 1.5
+BAN_THRESHOLD = 5
 MAX_FILE_MB = 45
 DOWNLOADS_DIR = "downloads"
-OPENAI_API_KEY = "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" 
+
+# OpenAI
+OPENAI_API_KEY = "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"
 PROJECT_NAME = "SylenthAgentBot"
